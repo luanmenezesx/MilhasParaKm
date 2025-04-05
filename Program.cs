e26fe8f66eeb321e3042f15ffc1af9b8d2f992c2
LuanGCMenezes <luangui2006@gmail.com>
2025-04-05 16:07:18 -0300

    Adicionando Exercício

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+double medidaEmMilhas, resultadoEmKm;
+
+Console.Write("Entre com a medida em (em milhas): ");
+
+medidaEmMilhas = Convert.ToDouble(Console.ReadLine()!);
+resultadoEmKm = medidaEmMilhas * 1.609;
+
+
+Console.WriteLine($"{resultadoEmKm}Km");
